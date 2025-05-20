d006519e9c36aacb7fb60247625a410afd607b6b
kezuk-dev <duprez.theo09@gmail.com>
2025-05-21 01:24:39 +0200

    Fix issues of ipforward bug

--- src/main/java/kezuk/akyto/listener/ServerListener.java
+++ src/main/java/kezuk/akyto/listener/ServerListener.java
@@ -1,7 +1,12 @@
 package kezuk.akyto.listener;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 import kezuk.akyto.BungeeCore;
 import net.md_5.bungee.api.ChatColor;
@@ -9,8 +14,10 @@ import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.PendingConnection;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
+import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
@@ -25,27 +32,63 @@ public class ServerListener implements Listener {
 	    ServerPing ping = e.getResponse();
 	    ServerInfo practice = this.main.getServer().getServerInfo("practice");
 	    if (practice.getMotd().contains("whitelisted")) {
-	    	e.getResponse().setDescriptionComponent(new TextComponent("                   " + ChatColor.DARK_GRAY + ChatColor.BOLD + "Akyto" + ChatColor.GRAY + " X " + ChatColor.DARK_AQUA + ChatColor.BOLD + "Soup" + ChatColor.AQUA + "World " + ChatColor.GRAY + "\n" + ChatColor.RED + " » " + ChatColor.GRAY + "Currently under development, coming soon" + ChatColor.WHITE + "..."));
+	    	e.getResponse().setDescriptionComponent(new TextComponent(ChatColor.translateAlternateColorCodes('&', "          &7• &8&lAkyto &7│ &f&lNetwork &7•\n&c» &7We are currently cooking right now...")));
 		    ServerPing.Protocol vers = ping.getVersion();
 		    vers.setName(ChatColor.GOLD.toString() + ChatColor.ITALIC + "Whitelisted");
 		    vers.setProtocol(9999);	
 	    }
-	    if (!practice.getMotd().contains("whitelisted")) e.getResponse().setDescriptionComponent(new TextComponent("                 " + ChatColor.DARK_GRAY + ChatColor.BOLD + "Akyto" + ChatColor.GRAY + " X " + ChatColor.DARK_AQUA + ChatColor.BOLD + "Soup" + ChatColor.AQUA + "World " + ChatColor.GRAY + "\n" + ChatColor.RED + " » " + ChatColor.GRAY + "Join us " + ChatColor.WHITE + "now!"));
+	    if (!practice.getMotd().contains("whitelisted")) e.getResponse().setDescriptionComponent(new TextComponent(ChatColor.translateAlternateColorCodes('&', "          &7• &8&lAkyto &7│ &f&lNetwork &7•\n&c» &fJoin our &f&lV2.0 &fright now!")));
 	    e.setResponse(ping);
 	}
-	
-    @EventHandler
-    public void onPlayerPreLogin(PreLoginEvent event) {
-        PendingConnection connection = event.getConnection();
-        InetSocketAddress address = connection.getAddress();
-        if (isLocalhost(address)) {
-            event.setCancelled(true);
-            event.setCancelReason("You cannot connected on Akyto with your localhost please connect with akyto.club");
-        }
-    }
+
+	@EventHandler
+	public void onPlayerPreLogin(PreLoginEvent event) {
+		PendingConnection connection = event.getConnection();
+		InetSocketAddress address = connection.getAddress();
+		if (isLocalhost(address)) {
+			event.setCancelled(true);
+			event.setCancelReason("You cannot connected on Akyto with your localhost please connect with akyto.club");
+		}
+		String serverIP = getMachineIP();
+		System.out.println("[Bungee] Machine IP: " + serverIP);
+
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		DataOutputStream out = new DataOutputStream(byteStream);
+		try {
+			out.writeUTF("MACHINE_IP");
+			out.writeUTF(serverIP);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		for (ServerInfo server : this.main.getProxy().getServers().values()) {
+			server.sendData("BungeeCord", byteStream.toByteArray());
+		}
+	}
 
     private boolean isLocalhost(InetSocketAddress address) {
         InetAddress inetAddress = address.getAddress();
         return inetAddress.isLoopbackAddress() || inetAddress.isAnyLocalAddress();
     }
+
+	private String getMachineIP() {
+		try {
+			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+			while (interfaces.hasMoreElements()) {
+				NetworkInterface iface = interfaces.nextElement();
+				if (iface.isLoopback() || !iface.isUp() || iface.isVirtual()) continue;
+
+				Enumeration<InetAddress> addresses = iface.getInetAddresses();
+				while (addresses.hasMoreElements()) {
+					InetAddress addr = addresses.nextElement();
+					if (!addr.isLoopbackAddress() && !addr.isAnyLocalAddress() && addr.getHostAddress().indexOf(":") == -1) {
+						return addr.getHostAddress(); // IPv4 only
+					}
+				}
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		return "unknown";
+	}
 }
